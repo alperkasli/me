625fdacb86bb9ce3af79e7a0b6ae1c7fd6b0972f
alperkasli <alperkasli@gmail.com>
2020-08-08 07:53:15 +0400

    changes

--- build/precache-manifest.c5862b7857cf65f4ed9134f23df234e6.js
+++ build/precache-manifest.c5862b7857cf65f4ed9134f23df234e6.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5c4fa050feaeaf5131a0d7b2c2ae83f5",
+    "revision": "1cfd0d980554197a67636627d525e7a9",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "2d69ea3e5dc8da676d53",
+    "revision": "f35e9d19c516c1be302c",
     "url": "/portfolio/static/css/main.3ce7d358.chunk.css"
   },
   {
-    "revision": "23c3c2c37ce83028ad89",
-    "url": "/portfolio/static/js/2.4044e07f.chunk.js"
+    "revision": "aac3160c87d712fabccc",
+    "url": "/portfolio/static/js/2.82eea301.chunk.js"
   },
   {
     "revision": "870030073881816e703f5df3e588856e",
-    "url": "/portfolio/static/js/2.4044e07f.chunk.js.LICENSE.txt"
+    "url": "/portfolio/static/js/2.82eea301.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "2d69ea3e5dc8da676d53",
-    "url": "/portfolio/static/js/main.849f4c5b.chunk.js"
+    "revision": "f35e9d19c516c1be302c",
+    "url": "/portfolio/static/js/main.f6265cfd.chunk.js"
   },
   {
     "revision": "7a6cc99bd8f08f9fa6ec",
