f0eae78a21e96d0e303a46806a4fcf911f79c881
alperkasli <alperkasli@gmail.com>
2020-08-08 07:08:00 +0400

    Updates

---
+++ precache-manifest.ff79a25c48712f4a2402dbf5211352a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c4fa050feaeaf5131a0d7b2c2ae83f5",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "2d69ea3e5dc8da676d53",
+    "url": "/portfolio/static/css/main.3ce7d358.chunk.css"
+  },
+  {
+    "revision": "23c3c2c37ce83028ad89",
+    "url": "/portfolio/static/js/2.4044e07f.chunk.js"
+  },
+  {
+    "revision": "870030073881816e703f5df3e588856e",
+    "url": "/portfolio/static/js/2.4044e07f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d69ea3e5dc8da676d53",
+    "url": "/portfolio/static/js/main.849f4c5b.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
